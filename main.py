476a518aee25616a6cd0ccfef1a1b6f72b65aaa8
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-03-26 23:07:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime, timezone
 
 MY_LAT = 38.627003 # Your latitude
 MY_LONG = -90.199402 # Your longitude
@@ -10,22 +11,25 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-print(data)
+# print(data)
 
 #WHAT TIME IS SUNRISE AND SUNSET IN STL METRO AREA???
 
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
-    "formatted": 1,
+    "formatted": 0
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = (data["results"]["sunrise"])
-sunset = (data["results"]["sunset"])
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 day_length = (data["results"]["day_length"])
-print(sunrise)
-print(sunset)
-print(f"The length of the day is {day_length}")
+print(f"The current sunrise in the STL Metro area is: {sunrise} UTC")
+print(f"The current sunset in the STL Metro area is: {sunset} UTC")
+# print(f"The length of the day is {day_length}")
+
+time_now = datetime.now(timezone.utc)
+print(f"The current time is: {time_now} UTC")
